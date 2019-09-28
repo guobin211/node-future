5899a161ae9e19b99d6d66170525bf4812425efe
GuoBin <guobin201314@gmail.com>
2019-09-28 16:11:05 +0800

    创建对象的效率

--- src/assert/person.test.js
+++ src/assert/person.test.js
@@ -4,3 +4,26 @@ console.log(person);
 const car = new Car();
 console.log(car);
 car.run('farrari', 430);
+
+let name = 'jack';
+name = name + '2019';
+console.log(name);
+
+console.time('jack');
+const jack = {
+  name: 'jack'
+};
+console.timeEnd('jack');
+
+console.time('jjack');
+const jjack = JSON.parse(`{"name": "jack"}`);
+console.timeEnd('jjack');
+
+console.time('jjjack');
+const jjjack = Object.create(null);
+jjjack.name = 'jack';
+console.timeEnd('jjjack');
+
+console.log(jjjack);
+
+Object.keys(jjjack).forEach(key => console.log(key));
