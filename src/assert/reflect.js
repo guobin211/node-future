2bb036bb4d5aa3e3df1bb2a4e32daf0432e4e6b8
GuoBin <guobin201314@gmail.com>
2019-10-25 11:08:30 +0800

    reflect

---
+++ src/assert/reflect.js
@@ -0,0 +1,29 @@
+/*
+* Reflect 是一个内置的对象，它提供拦截 JavaScript 操作的方法
+* */
+Reflect.metadata = {
+  'name': 'metadata'
+}
+
+console.log(Reflect)
+
+/*
+* 输出: 1 2 3 4
+* */
+function nativeLog(...args) {
+  console.log(...args)
+}
+
+const num = [1, 2, 3, 4]
+
+/*
+* 3种函数调用的方式
+* */
+nativeLog(...num)
+nativeLog.apply(null, num)
+Reflect.apply(nativeLog, null, num)
+
+let receve;
+receve = Reflect.get(num, 0)
+// 1
+console.log(receve)
