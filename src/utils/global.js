9b9d2a798a761878427e71a65ec16daacff5b0dc
GuoBin <guobin201314@gmail.com>
2019-11-06 09:21:05 +0800

    fix

---
+++ src/utils/global.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+/*
+* clearInterval
+  clearTimeout
+  setInterval
+  setTimeout
+  queueMicrotask
+  clearImmediate
+  setImmediate
+* */
+// console.log(global)
+console.log(global.queueMicrotask)
+
+let i = 1
+
+function printTask() {
+  console.log(i++)
+}
+
+/*
+* 宏任务
+* 1. setTimeout setInterval
+* 2. setImmediate requestAnimationFrame
+* 3. I/O
+* */
+
+fs.readFile('./es2018.js', () => {
+  console.log('node I/O')
+  printTask();
+})
+
+setImmediate(() => {
+  console.log('setImmediate')
+  printTask()
+})
+
+
+setTimeout(() => {
+  console.log('setTimeout')
+  printTask()
+})
+
+
+
+/*
+* 微任务
+* 1. process.nextTick
+* 2. Promise queueMicrotask  MutationObserver
+* */
+
+global.queueMicrotask(() => {
+  console.log('queueMicrotask')
+  printTask()
+})
+
+Promise.resolve().then(() => {
+  console.log('Promise')
+  printTask();
+})
+
+process.nextTick(() => {
+  console.log('process.nextTick')
+  printTask();
+})
+
+printTask();
+
+
---
+++ src/utils/is-number.js
@@ -0,0 +1,14 @@
+
+/**
+ * isLikeNumber
+ * @param val
+ * @returns {boolean}
+ */
+function isLikeNumber(val) {
+  return typeof val === 'number' || !isNaN(parseFloat(val));
+}
+
+const data = [-1, 0, 1, '2e', '-22', '23', 'e2']
+for (const datum of data) {
+  console.log(isLikeNumber(datum))
+}
