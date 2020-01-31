7ff104f7f9c7f2ca6d00c1ae1ae35090519691e0
GuoBin <guobin201314@gmail.com>
2020-01-31 09:39:15 +0800

    download video

---
+++ src/utils/download.js
@@ -0,0 +1,19 @@
+function findVideo() {
+  const video = document.querySelector('video')
+  if (video.firstChild) {
+    return video.firstChild.src
+  } else {
+    return video.src
+  }
+}
+
+function download(src) {
+  const a = document.createElement('a')
+  a.download
+  a.href = src
+  a.click()
+}
+
+function downloadVideo() {
+  return download(findVideo())
+}
