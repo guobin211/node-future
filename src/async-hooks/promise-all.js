73c8977bd0aa7a330c7921f093f773caa92efd23
GuoBin <guobin201314@gmail.com>
2020-07-15 22:01:22 +0800

    feat(async): promise all

---
+++ src/async-hooks/promise-all.js
@@ -0,0 +1,24 @@
+const promise1 = new Promise(resolve => {
+  setTimeout(() => {
+    resolve("promise1")
+  })
+})
+
+const promise2 = new Promise(resolve => {
+  setTimeout(() => {
+    resolve("promise2")
+  })
+})
+
+
+const promiseAll = [promise1, promise2]
+
+function getData(times) {
+  Promise.all(promiseAll).then(arr => {
+    console.log(`Promise.all ${times}`, arr)
+  })
+}
+
+getData(1)
+
+getData(2)
