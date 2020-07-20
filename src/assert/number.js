c27380395b0a1c6effa0f3d9043c8bf842bfe416
GuoBin <guobin201314@gmail.com>
2020-07-20 14:36:11 +0800

    主线程和子线程

---
+++ src/assert/number.js
@@ -0,0 +1,8 @@
+const a = 5 + "5"
+const b = "5" + 5
+const c = 5 - "5"
+const d = "5" - 5
+const e = "5" * 5
+const f = "5" / 5
+
+console.log(a, b, c, d, e, f)
---
+++ src/task/worker-thread.js
@@ -0,0 +1,21 @@
+const { Worker, isMainThread } = require("worker_threads")
+let res = 0;
+let subRes = 0;
+if (isMainThread) {
+  console.log("在主线程中")
+  res = sum(10, 20)
+  const wk = new Worker(__filename)
+  wk.addListener("exit", (exitCode) => {
+    console.log(res + subRes)
+  })
+} else {
+  console.log("在工作线程中")
+  subRes = sum(1, 2)
+  console.log(subRes)
+}
+
+function sum(a, b) {
+  console.log("js程序执行")
+  return a + b
+}
+
