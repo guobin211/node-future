31a13f160e351d58ba39a8491d9bd948225e75b1
GuoBin <guobin201314@gmail.com>
2020-07-21 10:28:11 +0800

    reflect反射代理

---
+++ src/utils/reflect.js
@@ -0,0 +1,28 @@
+// Reflect对象与Proxy对象一样，也是 ES6 为了操作对象而提供的新 API
+
+for (const key in Object.keys(Reflect)) {
+  console.log(key)
+}
+
+const person = {
+  name: "jack",
+  age: 22
+}
+
+if (Reflect.defineProperty(person, "name", {
+  configurable: true,
+  enumerable: true,
+  get() {
+    return this.value
+  },
+  set(v) {
+    this.value = v;
+  }
+})) {
+  console.log("define success", person)
+  console.log(person.name)
+  person.name = "tom"
+  console.log(person.name)
+} else {
+  console.log("error")
+}
