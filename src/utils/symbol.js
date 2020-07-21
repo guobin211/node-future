48634706237da452744450fa35f24d594c810e74
GuoBin <guobin201314@gmail.com>
2020-07-21 10:28:29 +0800

    symbol代理

---
+++ src/utils/symbol.js
@@ -0,0 +1,19 @@
+const SINGLE_TON_KEY = Symbol("SingleTon")
+
+class SingleTon {
+
+  constructor(name) {
+    this._name = name
+  }
+
+  play() {
+    console.log(`i an ${this._name} playing...`)
+  }
+
+}
+// 确保单例对象存在
+if (!global[SINGLE_TON_KEY]) {
+  global[SINGLE_TON_KEY] = new SingleTon("jack")
+}
+
+module.exports = global[SINGLE_TON_KEY]
