771f1d6bef65a6c944eb41e9786c24a471131864
GuoBin <guobin201314@gmail.com>
2020-07-21 10:27:43 +0800

    new关键字

---
+++ src/web/new-fun.js
@@ -0,0 +1,18 @@
+function newFn(fn, ...args) {
+  const obj = Object.create(fn.prototype)
+  const res = fn.apply(obj, args)
+  return res instanceof Object ? res: obj
+}
+
+function Person(name) {
+  this.name = name
+}
+
+Person.prototype.say = function () {
+  console.log("hello " + this.name)
+}
+
+const p1 = new Person("jack")
+const p2 = newFn(Person, "jack")
+p1.say()
+p2.say()
