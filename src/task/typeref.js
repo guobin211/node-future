b9e77ae85b647e31958fcf18f4de057724e04b4a
GuoBin <guobin201314@gmail.com>
2020-07-21 10:28:49 +0800

    类型转换

---
+++ src/task/typeref.js
@@ -0,0 +1,50 @@
+var b = 10;
+
+(function b() {
+  // eslint-disable-next-line no-func-assign
+  b = 20
+  // [Function: b]
+  console.log(b)
+})();
+
+(function () {
+  console.log(b)
+  b = 20
+  console.log(b)
+})()
+
+var a = {
+  i: 1,
+  toString() {
+    return a.i++
+  }
+};
+
+if (a == 1 && a == 2 && a == 3) {
+  console.log(1)
+}
+
+a = [1,2,3]
+a.join = a.shift;
+if (a == 1 && a == 2 && a == 3) {
+  console.log(1)
+}
+
+a = {
+  i: 1,
+  [Symbol.toPrimitive]: () => {
+    return a.i++
+  }
+}
+
+if (a == 1 && a == 2 && a == 3) {
+  console.log(1)
+}
+
+
+a = {
+  i: 1,
+  valueOf() {
+    return a.i++
+  }
+}
