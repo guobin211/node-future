ec535d0f3251d1d44b010ae666a9987b4a239cf4
GuoBin <guobin201314@gmail.com>
2020-07-29 10:10:30 +0800

    feat(run): &&串行执行服务，&并行执行

---
+++ src/gis-map/js/index.js
@@ -0,0 +1,9 @@
+// 页面完全加载,包括img
+window.onload = function() {
+  console.log(new Date().getTime(), "onload")
+}
+// 当初始的 HTML 文档被完全加载和解析完成之后，DOMContentLoaded 事件被触发，而无需等待样式表、图像和子框架的完全加载。
+// 同步 JavaScript 会暂停 DOM 的解析
+document.addEventListener('DOMContentLoaded',function(){
+  console.log(new Date().getTime(), "DOMContentLoaded");
+});
--- src/net/net-client.js
+++ src/net/net-client.js
@@ -1,18 +1,20 @@
 "use strict"
 const net = require("net")
 
-const client = net.createConnection({ port: 3001 }, () => {
-  console.log("connected to server port 3001")
-  client.write("i am client \r\n")
-})
-
-client.on("data", buff => {
-  console.log(buff.toString())
-  client.end(() => {
-    console.log("client 断开链接")
+setTimeout(() => {
+  const client = net.createConnection({ port: 3001 }, () => {
+    console.log("connected to server port 3001")
+    client.write("i am client \r\n")
   })
-})
 
-client.on("end", () => {
-  console.log("disconnected from server")
-})
+  client.on("data", buff => {
+    console.log(buff.toString())
+    client.end(() => {
+      console.log("client 断开链接")
+    })
+  })
+
+  client.on("end", () => {
+    console.log("disconnected from server")
+  })
+}, 1000)
