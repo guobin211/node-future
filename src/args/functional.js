7cbc71c8a7b7f05c4666dad77908b0bc0da73071
GuoBin <guobin201314@gmail.com>
2020-08-05 15:54:51 +0800

    feat(functional): 函数式

---
+++ src/args/functional.js
@@ -0,0 +1,49 @@
+function functional(condition) {
+  const res = {
+    _status: false,
+    match: (code, callback) => {
+      if (code === condition && !res._status) {
+        callback()
+        res._status = true
+      }
+      return res
+    },
+    other: callback => {
+      if (!res._status) {
+        callback()
+      }
+    },
+  }
+  return res
+}
+
+functional(3)
+  .match(1, () => {
+    console.log("2")
+  })
+  .match(2, () => {
+    console.log("2")
+  })
+  .other(() => {
+    console.log("other")
+  })
+
+
+class Target {
+  constructor(condition) {
+    this.condition = condition
+    this.matched = false
+  }
+
+  match(code, callback) {
+    if (!this.matched && this.condition === code) {
+      this.matched = true
+      callback()
+    }
+    return this
+  }
+
+  other(callback) {
+    callback()
+  }
+}
